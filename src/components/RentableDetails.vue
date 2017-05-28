8c2cfb124d22837a0fc4ef883a54b230128a8c39
Andreas Schmid <andreas.schmid.as3@roche.com>
2017-05-28 18:14:20 +0200

    Improve RentableDetails example

--- src/components/RentableDetails.vue
+++ src/components/RentableDetails.vue
@@ -1,24 +1,68 @@
 <template>
   <div>
+
     <md-card>
-      <md-card-header>
-	<md-card-header-text>
-	  <div class="md-title">Rentable Number 1</div>
-          <div class="md-subhead">Address {{ $route.params.address }}</div>
-	  <div class="md-subhead">Location Buenzen</div>
-	  <div class="md-subhead">Description: bla bla</div>
-	</md-card-header-text>
+      <md-card-area md-inset>
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">Rentable Number 1</div>
+            <div class="md-subhead">Lorem ipsum asdfj ;salkdfj skdfj ldskfj sl;dfj s;dfj </div>
+          </md-card-header-text>
+          <md-card-media>
+            <img src="static/lokkit_icon_100.png" alt="logo">
+          </md-card-media>
+        </md-card-header>
 
-	<md-card-media>
-	  <img src="static/lokkit_icon_100.png" alt="logo">
-	</md-card-media>
-      </md-card-header>
+        <md-card-content>
+          <div class="card-reservation">
+            <md-icon md-iconset="mdi mdi-qrcode"></md-icon>
+            {{ $route.params.address }}
+          </div>
+          <div class="card-reservation">
+            <md-icon>location_on</md-icon>
+            Buenzen, top locker
+          </div>
+          <div class="card-reservation">
+            <md-icon>account_circle</md-icon>
+            0x57385756238729485840938457
+          </div>
+        </md-card-content>
+        <md-card-content>
+          <div class="md-warn">
+            <md-icon class="md-warn">info</md-icon>
+            You are currently not the renter of this rentable
+          </div>
+        </md-card-content>
+        <md-card-actions>
+          <md-button>Lock</md-button>
+        </md-card-actions>
+      </md-card-area>
+    </md-card>
 
+    <md-card style="margin-top:1em; margin-bottom:1em">
+      <md-card-area md-inset>
+        <md-card-content>
+          <h3 class="md-subheading" style="display:flex;">Reservations</h3>
+          <div class="card-reservation">
+            <md-icon>access_time</md-icon> 10:00 - 22:00
+            <md-icon style="margin-left: .5em;">account_circle</md-icon> 0x57385756238729485840938457
+          </div>
+          <div class="card-reservation">
+            <md-icon>access_time</md-icon> 10:00 - 22:00
+            <md-icon style="margin-left: .5em;">account_circle</md-icon> 0x57385756238729485840938457
+          </div>
+          <div class="card-reservation">
+            <md-icon>access_time</md-icon> 10:00 - 22:00
+            <md-icon style="margin-left: .5em;">account_circle</md-icon> 0x57385756238729485840938457
+          </div>
+        </md-card-content>
+      </md-card-area>
       <md-card-actions>
-	<md-button>Action</md-button>
-	<md-button>Action</md-button>
+	<md-button>Reserve</md-button>
       </md-card-actions>
     </md-card>
+
+
   </div>
 </template>
 
@@ -29,4 +73,16 @@
 </script>
 
 <style scoped>
+  .card-reservation {
+    margin-top: 8px;
+
+    .md-icon {
+      margin: 28px;
+      color: rgba(#000, .54) !important;
+    }
+
+    .md-button {
+      border-radius: 2px !important;
+    }
+  }
 </style>
