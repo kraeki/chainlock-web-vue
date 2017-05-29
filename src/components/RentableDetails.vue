43e614064b01b594c301f493271621f5e8144d09
Andreas Schmid <andreas.schmid.as3@roche.com>
2017-05-30 01:52:44 +0200

    Add ReserveDialog to enter reservation details

--- src/components/RentableDetails.vue
+++ src/components/RentableDetails.vue
@@ -45,31 +45,90 @@
         <md-card-content>
           <h3 class="md-subheading" style="display:flex;">Reservations</h3>
           <div class="card-reservation" v-for="reservation in rentable.reservations">
-            <md-icon>access_time</md-icon> {{reservation.start}} - {{reservation.end}}
+            <md-icon>access_time</md-icon> {{reservation.start | formatUnixTimeStamp}} - {{reservation.end | formatUnixTimeStamp}}
             <md-icon style="margin-left: .5em;">account_circle</md-icon> {{reservation.renter}}
           </div>
         </md-card-content>
       </md-card-area>
       <md-card-actions>
-	<md-button @click.native="reserve">Reserve</md-button>
+	<md-button @click.native="openDialog('reserveDialog')">Reserve</md-button>
       </md-card-actions>
     </md-card>
 
+    <md-dialog ref="reserveDialog">
+      <md-dialog-title>Add reservation</md-dialog-title>
 
+      <md-dialog-content>
+        <form>
+          <md-input-container>
+            <md-icon>event</md-icon>
+            <label>Date</label>
+            <md-input v-model="reserveDialog.fromDate" type="date"></md-input>
+          </md-input-container>
+          <md-input-container>
+            <md-icon>schedule</md-icon>
+            <label>Time</label>
+            <md-input v-model="reserveDialog.fromTime" type="time"></md-input>
+          </md-input-container>
+
+          <md-layout md-gutter>
+            <md-layout>
+              <md-input-container>
+                <md-icon>update</md-icon>
+                <label>Duration</label>
+                <md-input v-model="reserveDialog.duration" type="number"></md-input>
+              </md-input-container>
+            </md-layout>
+            <md-layout md-flex="33" md-flex-offset="33">
+              <md-input-container>
+                <label for="unit">Unit</label>
+                <md-select name="unit" id="unit" v-model="reserveDialog.unit">
+                  <md-option value="Minutes">Minutes</md-option>
+                  <md-option value="Hours">Hours</md-option>
+                  <md-option value="Days">Days</md-option>
+                  <md-option value="Weeks">Weeks</md-option>
+                  <md-option value="Months">Months</md-option>
+                  <md-option value="Years">Years</md-option>
+                </md-select>
+              </md-input-container>
+            </md-layout>
+          </md-layout>
+        </form>
+      </md-dialog-content>
+
+      <md-dialog-actions>
+        <md-button class="md-primary" @click.native="closeDialog('reserveDialog')">Cancel</md-button>
+        <md-button class="md-primary" @click.native="reserve">Reserve</md-button>
+      </md-dialog-actions>
+    </md-dialog>
   </div>
 </template>
 
 <script>
+  import moment from 'moment'
+
   export default {
     name: 'RentableDetails',
+
     methods: {
+      openDialog: function (dialog) {
+        this.$refs[dialog].open()
+      },
+      closeDialog: function (dialog) {
+        this.$refs[dialog].close()
+      },
       reserve: function () {
+        // calculate from and to using parameters given by the reserveDialog
+        const from = moment(this.reserveDialog.fromDate + ' ' + this.reserveDialog.fromTime)
+        const to = from.clone().add(this.reserveDialog.duration, this.reserveDialog.unit)
+
         this.$store.dispatch('reserve', {
           rentableAddress: this.rentable.address,
-          start: '23:00',
-          end: '24:00',
+          start: from.unix(),
+          end: to.unix(),
           account: '0x99'
         })
+        this.closeDialog('reserveDialog')
       },
       lock: function () {
         this.$store.dispatch('lock', {
@@ -82,6 +141,17 @@
           rentableAddress: this.rentable.address,
           account: '0x99'
         })
+      }
+    },
+    data: function () {
+      const now = moment()
+      return {
+        reserveDialog: {
+          fromDate: now.format('YYYY-MM-DD'),
+          fromTime: now.format('HH:mm'),
+          unit: 'Minutes',
+          duration: 15
+        }
       }
     },
     computed: {
