37e3394e72c375a10d87a859a0a9a732d1d1cad6
Andreas Schmid <andreas.schmid.as3@roche.com>
2017-06-03 18:11:29 +0200

    Add new component to shorten address

---
+++ src/components/Address.vue
@@ -0,0 +1,33 @@
+<template>
+  <span>
+    {{addressStart}}
+    ...
+    <md-tooltip md-direction="top">{{address}}</md-tooltip>
+    {{addressEnd}}
+  </span>
+</template>
+
+<script>
+  export default {
+    name: 'lkAddress',
+    components: {},
+    props: {
+      address: {
+        type: String,
+        required: true
+      }
+    },
+    computed: {
+      addressStart () {
+        return this.address.substring(0, 8)
+      },
+      addressEnd () {
+        const length = this.address.length
+        return this.address.substring(length - 6)
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
