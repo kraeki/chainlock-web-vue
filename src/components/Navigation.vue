19170c93632f7b6e65653c874289ae341724454a
Andreas Schmid <andreas.schmid.as3@roche.com>
2017-06-04 00:54:48 +0200

    Cleanup: Remove obsolete components

--- src/components/Navigation.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-      <el-menu-item index="1"><i class="el-icon-menu"></i>Navigator Two</el-menu-item>
-      <el-menu-item index="2"><i class="el-icon-setting"></i>Navigator Three</el-menu-item>
-    </el-menu>
-  </div>
-</template>
